6e036015f75b0aa6ddf2fe33d07aff2efe3e7a33
henricbjork2liu <84723867+henricbjork2liu@users.noreply.github.com>
2021-06-18 10:38:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("will now loop through and print every letter in 'banana'"
+import os;
+
+print("will now loop through and print every letter in 'banana'")
 for x in "banana":
   print(x)
+
+print("Banana is now printed")
